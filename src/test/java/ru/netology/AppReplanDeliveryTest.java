e7a4ce855f6afb34614f69ac78d223852df3dc21
Aleksandr Stupnikov <a-a-stupnikov@yandex-team.ru>
2024-10-20 18:49:36 +0300

    initial commit

---
+++ src/test/java/ru/netology/AppReplanDeliveryTest.java
@@ -0,0 +1,47 @@
+package ru.netology;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AppReplanDeliveryTest {
+
+
+    String secondDate = Generator.dataGenerate(5);
+    String firstDate = Generator.dataGenerate(4);
+
+    String city = Generator.city();
+    String phone = Generator.phone();
+    String fullName = Generator.name();
+
+    @Test
+    public void validTestAppReplanDelivery() {
+
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        $("[data-test-id=date] input").setValue(firstDate);
+        $("[data-test-id=name] input").setValue(fullName);
+        $("[data-test-id=phone] input").setValue(phone);
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
+        $("[data-test-id=success-notification]").shouldHave(text("Встреча успешно запланирована на " + firstDate))
+                .shouldBe(visible);
+        $("[data-test-id=date] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        $("[data-test-id=date] input").setValue(secondDate);
+        $(".button__content").click();
+        $(withText("У вас уже запланирована встреча на другую дату.")).shouldBe(visible);
+        $("[data-test-id=replan-notification] .button__content").click();
+        $("[data-test-id=success-notification] .notification__content")
+                .shouldBe(exactText("Встреча успешно запланирована на " + secondDate), (visible));
+    }
+}
---
+++ src/test/java/ru/netology/Generator.java
@@ -0,0 +1,44 @@
+package ru.netology;
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class Generator {
+
+
+    static Faker faker = new Faker();
+
+    public static String name() {
+        Faker faker = new Faker(new Locale("ru"));
+
+        String fullName = faker.name().fullName();
+        return fullName;
+    }
+
+    public static String phone() {
+        Faker faker = new Faker(new Locale("ru"));
+
+        String PhoneClient = faker.name().fullName();
+        return PhoneClient;
+    }
+
+    public static String city() {
+        List<String> list = Arrays.asList("Волгоград", "Владивосток", "Владимир", "Вологда", "Рязань",
+                "Чебоксары", "Москва", "Санкт-Петербург", "Симферополь", "Ростов-на-Дону");
+        Random rand = new Random();
+        String city = list.get(rand.nextInt(list.size()));
+        return city;
+    }
+
+    public static String dataGenerate(int step) {
+        String date = LocalDate.now().plusDays(step).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+        return date;
+    }
+}
